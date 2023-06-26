8bbd0d3c44a003e838a0d1c4ba6631c2378fc4f2
Anis Emad <anisemad123@gmail.com>
2023-06-26 16:20:45 +0000

    update

---
+++ handle_functions.c
@@ -0,0 +1,106 @@
+#include "main.h"
+/**
+  * print_char - print a single character
+  * @c: the character used
+  * Return: void
+  */
+void print_char(char c)
+{
+	write(STDOUT_FILENO, &c, 1);
+}
+/**
+  * print_string - print a string
+  * @s: pointer to a string
+  * Return: void
+  */
+void print_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		char c = s[i];
+
+		print_char(c);
+		i++;
+	}
+}
+/**
+  * print_integer - print a decimal number
+  * @number: number used
+  * Return: void
+  */
+void print_integer(int number)
+{
+	char buffer[20];
+	int length = 0;
+	int temp = number;
+	int is_negative = 0;
+	int i;
+
+	if (temp == 0)
+	{
+		buffer[length++] = '0';
+	}
+	else if (temp < 0)
+	{
+		is_negative = 1;
+		temp = -temp;
+	}
+	while (temp != 0)
+	{
+		buffer[length++] = '0' + (temp % 10);
+		temp /= 10;
+	}
+
+	if (is_negative)
+	{
+		buffer[length++] = '-';
+	}
+	for (i = 0; i < length / 2; i++)
+	{
+		char temp = buffer[i];
+
+		buffer[i] = buffer[length - i - 1];
+		buffer[length - i - 1] = temp;
+	}
+
+	write(STDOUT_FILENO, buffer, length);
+}
+/**
+  * to_decimal - converts the number to decimal
+  * @number: the number used
+  * @flag: tell us if it's hexa or octal
+  * Return: converted number
+  */
+int to_decimal(int number, int flag)
+{
+	int decimal_num = 0;
+
+	if (flag == 0)
+	{
+		int power = 0;
+
+		while (number > 0)
+		{
+			int digit = number % 10;
+
+			decimal_num += digit * (1 << (3 * power));
+			number /= 10;
+			power++;
+		}
+	}
+	else
+	{
+	}
+	return (decimal_num);
+}
+/**
+  * handle_i - handles numbers when it's i
+  * @number: the number handled
+  * Return: void
+  */
+void handle_i(int number)
+{
+	;
+}
--- printf.c
+++ printf.c
@@ -11,44 +11,45 @@
  * Return:
  * The number of characters printed on success, or -1 on error.
  **/
- int _printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
+	va_list args;
+	int i;
+	char c;
+	char *s;
+	int d, ii;
 
-    int i;
-
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                char c = va_arg(args, int);
-                print_char(c);
-            }
-            else if (format[i] == 's')
-            {
-                char *c = va_arg(args, char *);
-                print_string(c);
-            }
-            else if (format[i] == '%')
-            {
-                print_char('%');
-            }
-            else if (format[i] == 'd')
-            {
-                int d = va_arg(args, int);
-                print_integer(d);
-            }
-            else if (format[i] == 'i')
-            {
-                int i = va_arg(args, int);
-                handle_i(i);
-            }
-        } else
-            print_char(format[i]);
-    }
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				print_char(c);
+			}
+			else if (format[i] == 's')
+			{
+				s = va_arg(args, char *);
+				print_string(c);
+			}
+			else if (format[i] == '%')
+			{
+				print_char('%');
+			}
+			else if (format[i] == 'd')
+			{
+				d = va_arg(args, int);
+				print_integer(d);
+			}
+			else if (format[i] == 'i')
+			{
+				ii = va_arg(args, int);
+				handle_i(ii);
+			}
+		} else
+			print_char(format[i]);
+	}
 }
-

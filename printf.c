d41bcb468caf93b57901653bd0946e8ff7d84129
Anis Emad <anisemad123@gmail.com>
2023-06-26 19:03:44 +0000

    update

--- printf.c
+++ printf.c
@@ -1,37 +1,49 @@
 #include "main.h"
 /**
- * handle -handle specifier 
+ * handle -handle specifier
  * @p:decision parameter
  * return : numbers of characters
  **/
-int handle(char p)
+int handle(const char *format, va_list args)
 { 
-	int sum = 0;
-                        if (p == 'c')
-                        {
-                                c = va_arg(args, int);
-                                sum += print_char(c);
-                        }
-                        else if (p == 's')
-                        {
-                                s = va_arg(args, char *);
-                                sum += print_string(s);
-                        }
-                        else if (p == '%')
-                        {
-                                sum += print_char('%');
-                        }
-                        else if (p == 'd')
-                        {
-                                d = va_arg(args, int);
-                                sum += print_integer(d);
-                        }
-                        else if (p == 'i')
-                        {
-                                ii = va_arg(args, int);
-				       sum += handle_i(ii);
-                        }
-			return (sum);
+	int sum = 0, i, d;
+	char c, *s;
+	
+	while(*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				sum += print_char(c);
+			}
+			else if (*format == 's')
+			{
+				s = va_arg(args, char *);
+				sum += print_string(s);
+			}
+			else if (*format == '%')
+			{
+				sum += print_char('%');
+			}
+			else if (*format == 'd')
+			{
+				d = va_arg(args, int);
+				sum += print_integer(d);
+			}
+			else if (*format == 'i')
+			{
+				i = va_arg(args, int);
+				sum += handle_i(i);
+			}
+		}
+		else 
+			sum += print_char(*format);
+		format++;
+	}
+	return (sum);
 }
 /**
  * _printf - Custom printf function
@@ -47,9 +59,7 @@ int handle(char p)
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i;
-	char c, *s;
-	int d, ii, sum = 0;
+	int sum = 0;
                     
 	if (!format)
 	{
@@ -57,17 +67,8 @@ int _printf(const char *format, ...)
 	}
 
 	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			sum += handle(format[i];
-		} else
-			sum += print_char(format[i]);
-	}
+	handle(format, args);
 	va_end(args);
 	return (sum);
 }
-
 

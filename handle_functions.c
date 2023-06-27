5386392faef7d5242e46dedf5b130625641e5ffc
Jacqueline-malak <jacquelinemalak230@gmail.com>
2023-06-27 08:17:56 -0700

    hanle

--- handle_functions.c
+++ handle_functions.c
@@ -6,7 +6,18 @@
   */
 int print_char(char c)
 {
-	write(STDOUT_FILENO, &c, 1);
+	static char buf[1024];
+	static int i;
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}	
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
 	return (1);
 }
 /**
